ba7be11ce3f186e33e6fb12647b71426cb9ba2ff
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-06-29 15:16:28 +0530

    public static int[] game_with_number (int arr[], int n) {

---
+++ bitManupilation/PlayWithOR.java
@@ -0,0 +1,18 @@
+package bitManupilation;
+
+public class PlayWithOR {
+	
+	public static int[] game_with_number (int arr[], int n) {
+		 
+        for(int i = 0 ; i < n - 1 ; i++ ){
+            arr[i] = arr[i]|arr[i + 1];
+        }
+        return arr;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
