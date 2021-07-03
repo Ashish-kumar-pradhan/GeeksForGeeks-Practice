bed3197c29918664e197084466a9b34f626b5ebb
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-07-03 12:51:57 +0530

    public static void countPathDFS(ArrayList<ArrayList<Integer>> adj , int s, int d , boolean[] visited , int[] c) {

---
+++ graph/PossiblePathsBetween2Vertices.java
@@ -0,0 +1,41 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class PossiblePathsBetween2Vertices {
+	
+public static void countPathDFS(ArrayList<ArrayList<Integer>> adj , int s, int d , boolean[] visited , int[] c) {
+		
+		int n = adj.size();
+		visited[s] = true;
+		if(s == d) {
+			c[0]++;
+		}
+		else {
+    		ArrayList<Integer> list = adj.get(s);
+    		for(int i = 0 ; i < list.size() ; i++) {
+    			if(!visited[list.get(i)]) {
+    				countPathDFS(adj , list.get(i) , d , visited , c);
+    				visited[list.get(i)] = false;
+    			} 
+    		}
+		}
+	}
+
+    public int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source, int destination)
+    {
+        
+        boolean visited[] = new boolean[V];
+		int[] c = new int[1];
+		countPathDFS(adj , source , destination , visited , c );
+		
+		return c[0];
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
