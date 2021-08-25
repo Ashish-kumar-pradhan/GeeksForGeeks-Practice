2050c561f5fb5402ccf41d9c27e88f329d61ce06
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-08-25 17:50:17 +0530

    Implement Queue using array

---
+++ queue/ImplementQueueUsingArray.java
@@ -0,0 +1,41 @@
+package queue;
+
+public class ImplementQueueUsingArray {
+	
+	
+	int front, rear;
+	int arr[] = new int[100005];
+
+    MyQueue()
+	{
+		front=0;
+		rear=0;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int x)
+	{
+	    // Your code here
+	    arr[rear++] = x;
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{
+		// Your code here
+		if(front >= rear){
+		    return -1;
+		}
+		
+		else {
+		    return arr[front++];
+		}
+		
+	} 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
