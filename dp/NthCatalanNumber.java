14d5fa95f433a72b733e52db1f4c943a5fb3dea6
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-09-12 15:37:11 +0530

    Nth catalan number

--- dp/NthCatalanNumber.java
+++ dp/NthCatalanNumber.java
@@ -1,6 +1,24 @@
 package dp;
 
+import java.math.BigInteger;
+
 public class NthCatalanNumber {
+	
+	public static BigInteger findCatalan(int n)
+    {
+        //Your code here
+        BigInteger[] catalan = new BigInteger[101];
+        catalan[0] = BigInteger.ONE;
+        catalan[1] = BigInteger.ONE;
+        for(int i = 2 ; i <= n ; i++){
+            catalan[i] = BigInteger.ZERO;
+            for(int j = 0 ; j < i ; j++){
+                catalan[i] = catalan[i].add(catalan[j].multiply(catalan[i-j-1]));
+            }
+        }
+        
+        return catalan[n];
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
