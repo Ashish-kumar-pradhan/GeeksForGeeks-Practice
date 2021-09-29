bdf262a0909d839dde42c9ab6c02a75f5e98e3fe
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-09-29 14:14:08 +0530

    Number Of Open Doors

---
+++ mathematical/NumberOfOpenDoors.java
@@ -0,0 +1,16 @@
+package mathematical;
+
+public class NumberOfOpenDoors {
+	
+	static int noOfOpenDoors(Long N) {
+        // code here
+        int n = (int)(Math.pow(N , 0.5));
+        return n;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
